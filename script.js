0476c22ec2fc6076a412b546cabf5e33a0351aa4
David Martinez <jorgemart94@gmail.com>
2023-09-30 22:19:27 -0500

    Funcionalidad de los menus desplegables del Nav

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const email = document.querySelector('.email');
+const menuDesplegable = document.querySelector('.menuDesplegable');
+const carrito = document.querySelector('.carrito');
 const desktopMenu = document.querySelector('.desktopMenu');
 const movilMenu = document.querySelector('.movilMenu');
-const menuDesplegable = document.querySelector('.menuDesplegable');
+const containerMain = document.querySelector('.containerMain');
+// -------------------------------------navbar-------------------------------------
 
 function pliegueDespliegueMenuDesktop(){
+    const isCarritoOpen = !containerMain.classList.contains('inactive')
+    if(isCarritoOpen){
+        containerMain.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive')  
 }
 email.addEventListener('click', pliegueDespliegueMenuDesktop)
 
 function pliegueDespliegueMenuMovile(){
+    const isCarritoOpen = !containerMain.classList.contains('inactive')
+    if(isCarritoOpen){
+        containerMain.classList.add('inactive')
+    }
     movilMenu.classList.toggle('inactive')  
 }
 menuDesplegable.addEventListener('click', pliegueDespliegueMenuMovile)
 
-
+// -----------------------------------aside-carrito-----------------------------------
+function pliegueDesplieguecarrito(){
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive')
+    const isMovilMenuOpen = !movilMenu.classList.contains('inactive')
+    if(isDesktopMenuOpen || isMovilMenuOpen){
+        desktopMenu.classList.add('inactive')
+        movilMenu.classList.add('inactive')
+    }
+    containerMain.classList.toggle('inactive')  
+}
+carrito.addEventListener('click', pliegueDesplieguecarrito)
